d458c45534b1e5dab1b533850f86cae3c1b323fe
karamjit-2660 <37602@esjaloures.org>
2026-06-14 20:01:49 +0100

    metei novo mailtrap.

--- app/config/mail.php
+++ app/config/mail.php
@@ -2,8 +2,8 @@
 return [
     'host' => 'sandbox.smtp.mailtrap.io',
     'port' => 2525,          // ou 587 / 465 conforme o Mailtrap
-    'username' => '49a391af0a2da4',
-    'password' => 'a8f365fbc7014c',
+    'username' => 'a4a7a483f2e0c1',
+    'password' => '9eaf4feb9f128a',
     'encryption' => 'tls',   // 'tls' (STARTTLS) normalmente
     'from_email' => 'no-reply@mydevscreenalert.com',
     'from_name' => 'Meu Site',
